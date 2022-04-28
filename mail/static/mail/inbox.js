f906a9203a315400bcea7b75ae1f6f32a690511f
Ong Zheng Liang <joesurf@Joesurfs-MacBook.local>
2022-04-28 09:47:48 +0800

    animate & scroll

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -153,3 +153,12 @@ function load_mailbox(mailbox) {
     });
   })
 }
+
+// Add scroll properties
+window.onscroll = () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    document.querySelector('body').style.background = 'green';
+  } else {
+    document.querySelector('body').style.background = 'white';
+  }
+}
